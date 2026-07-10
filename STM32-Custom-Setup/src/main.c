7a61487c64ca532dd4a59e0a4be9076f9f2c7a64
Jonas Thaun <thaunjonas@gmail.com>
2026-07-10 21:02:12 +0200

    timer encoder

--- STM32-Custom-Setup/src/main.c
+++ STM32-Custom-Setup/src/main.c
@@ -1,47 +1,41 @@
 #include <stdint.h>
 
 #define RCC_AHB1ENR (*(volatile uint32_t *)(0x40023800 + 0x30)) // ahb1 enable reg
-#define GPIOC_MODER (*(volatile uint32_t *)(0x40020800 + 0x00)) // port c mode reg
-#define GPIOC_IDR (*(volatile uint32_t *)(0x40020800 + 0x10))   // input data reg
+#define RCC_APB1ENR  (*(volatile uint32_t *)(0x40023800 + 0x40)) //apb1 enable reg
+
+#define GPIOC_MODER  (*(volatile uint32_t *)(0x40020800 + 0x00)) // port c mode reg
+#define GPIOC_PUPDR  (*(volatile uint32_t *)(0x40020800 + 0x0C)) //port c pullup pulldown reg
+#define GPIOC_AFRL   (*(volatile uint32_t *)(0x40020800 + 0x20)) //port c alternate function reg
+
+#define TIM3_CR1     (*(volatile uint32_t *)(0x40000400 + 0x00)) //timer3 control reg
+#define TIM3_SMCR    (*(volatile uint32_t *)(0x40000400 + 0x08)) //timer3 slave mode control reg
+#define TIM3_CCMR1   (*(volatile uint32_t *)(0x40000400 + 0x18)) //timer3 capture compare mode reg
+#define TIM3_CNT     (*(volatile uint32_t *)(0x40000400 + 0x24)) //timer3 count reg
+#define TIM3_ARR     (*(volatile uint32_t *)(0x40000400 + 0x2C)) //timer3 auto reload register
 
 int main(void)
 {
-    RCC_AHB1ENR |= (1 << 1) | (1 << 2);
-    GPIOC_MODER &= ~((3 << 0) | (3 << 2) | (3 << 4));
+    RCC_AHB1ENR |= (1U << 2); // GPIOC clock enable
+    RCC_APB1ENR |= (1U << 1); // TIM3 clock enable
 
-    uint8_t last_state_A = 1;
-    volatile int32_t counter = 0;
+    GPIOC_MODER &= ~((3U << 12) | (3U << 14));  //reset mode 
+    GPIOC_MODER |=  ((2U << 12) | (2U << 14)); //set alternate function (10)
 
-    while (1)
-    {
-        uint8_t current_A = (GPIOC_IDR & (1 << 0)) ? 1 : 0;
-        uint8_t current_B = (GPIOC_IDR & (1 << 1)) ? 1 : 0;
-        uint8_t button = (GPIOC_IDR & (1 << 2)) ? 1 : 0;
+    GPIOC_PUPDR &= ~((3U << 12) | (3U << 14)); //reset pullup/down 
+    GPIOC_PUPDR |=  ((1U << 12) | (1U << 14)); //set pullups (01)
 
-        // pin a falling edge
-        if ((last_state_A == 1) && (current_A == 0))
-        {
-            // b 1 = right, b 0 = left
-            if (current_B == 1)
-            {
-                counter++;
-            }
-            else
-            {
-                counter--;
-            }
+    GPIOC_AFRL  &= ~((0xFU << 24) | (0xFU << 28)); //clear alrernate function mapping
+    GPIOC_AFRL  |=  ((2U << 24) | (2U << 28)); //set af 
 
-            // debounce delay
-            for (volatile int i = 0; i < 50000; i++)
-            {
-            }
-        }
+    TIM3_ARR = 0xFFFF; //set max auto reload
 
-        last_state_A = current_A;
+    TIM3_CCMR1 = (1U << 0) | (1U << 8) | (0xFU << 4) | (0xFU << 12); //map pins to timer & add max filtwe
+    TIM3_SMCR = (3U << 0); //encoder mode 3 (count edges)
+    TIM3_CR1 = (1U << 0); //counter enable
 
-        if (button == 0)
-        {
-            counter = 0;
-        }
+    volatile int32_t encoder_position = 0;
+
+    while(1) {
+        encoder_position = ((int32_t)(int16_t)TIM3_CNT) / 4; //4 esges counted (2 per pin)
     }
 }
